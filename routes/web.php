b667076321f0c1239a7bc34e12a062a6275d4704
Jardel Mitermayer <jardel.filho@dcomp.ufs.br>
2021-05-26 03:30:47 -0300

    feat:adicionando login

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/produtos', 'App\Http\Controllers\ProdutoControlador@index');
+
+Route::post('/login', function (Request $request) {
+
+    $login_ok = false;
+
+    switch($request->input('user')) {
+        case 'joao': 
+            $login_ok = $request->input('password') === "senhajoao";
+            break;
+        case 'marcos': 
+            $login_ok = $request->input('password') === "senhamarcos";
+            break;
+        case 'default':
+            $login_ok = false;
+    }
+    if($login_ok){
+        return response("Logado", 200);
+    }else{
+        return response("Erro no login", 404);
+    }
+});
