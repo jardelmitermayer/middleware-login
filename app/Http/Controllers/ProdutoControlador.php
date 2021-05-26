1bb6eaf77d0e99d93c61769b48bb9f12d2d7b4d0
Jardel Mitermayer <jardel.filho@dcomp.ufs.br>
2021-05-26 03:20:41 -0300

    feat:criando projeto, controladores e rotas

---
+++ app/Http/Controllers/ProdutoControlador.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProdutoControlador extends Controller
+{
+    private $produtos = ["Televisao 40", "Notebook Acer", "Impressora HP", "HD Externo"];
+    public function index () {
+        echo "<h3>Produtos</h3>";
+        echo "<ol>";
+        foreach($this->produtos as $prod){
+            echo "<li>" . $prod . "</li>";
+        }
+        echo "</ol>";
+    }
+}
