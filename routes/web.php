a69169b04c18f15c9a5bdae42a1cf8d754057c3e
Jardel Mitermayer <jardel.filho@dcomp.ufs.br>
2021-05-26 04:09:45 -0300

    feat:verificando se o usuário é admin ou não

--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,21 @@ Route::get('/', function () {
 Route::get('/produtos', 'App\Http\Controllers\ProdutoControlador@index');
 
 Route::get('/negado', function () {
-    return "Acesso negado";
+    return "Acesso negado. Você precisa estar logado para acessar essa página";
 })->name('negado');
 
+Route::get('/negadologin', function () {
+    return "Oops. Você precisa ser administrador para acessar esse conteúdo";
+})->name('negadologin');
 
 Route::post('/login', function (Request $request) {
 
     $login_ok = false;
-
+    $admin = false;
     switch($request->input('user')) {
         case 'joao': 
             $login_ok = $request->input('password') === "senhajoao";
+            $admin = true;
             break;
         case 'marcos': 
             $login_ok = $request->input('password') === "senhamarcos";
@@ -29,7 +33,7 @@ Route::post('/login', function (Request $request) {
             $login_ok = false;
     }
     if($login_ok){
-        $login = ['user'=> $request->input('user')];
+        $login = ['user'=> $request->input('user'), 'admin' => $admin];
         $request->session()->put('login', $login);
         return response("Logado", 200);
     }else{
